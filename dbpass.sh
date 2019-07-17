ba298661f52754432df02ffa57b3a46abacffc63
renzhendeyu <39947336+renzhendeyu@users.noreply.github.com>
2019-07-17 10:53:10 +0800

    修改数据库的密码
    接受八个参数

--- dbpass.sh
+++ dbpass.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-MYSQL_PASS=$1
-KEYSTONE_DBPASS=$2
-GLANCE_DBPASS=$3
-NOVA_DBPASS=$4
-PLACEMENT_DBPASS=$5
-NEUTRON_DBPASS=$6
-DASH_DBPASS=$7
-CINDER_DBPASS=$8
+MYSQL_PASS=$1           #数据库密码
+KEYSTONE_DBPASS=$2      #keystone数据库密码
+GLANCE_DBPASS=$3        #glance数据库密码
+NOVA_DBPASS=$4          #nova数据库密码
+PLACEMENT_DBPASS=$5     #placement数据库密码
+NEUTRON_DBPASS=$6       #neutron数据库密码
+DASH_DBPASS=$7          #dashboard数据库密码
+CINDER_DBPASS=$8        #cinder数据库密码
 echo $MYSQL_PASS | sed -i "s/MYSQL_PASS/${MYSQL_PASS}/g" /root/openstack/installglance.sls /root/openstack/installcinder_master.sls /root/openstack/installkeystone.sls /root/openstack/installnova_master.sls /root/openstack/installneu_master.sls
 echo $KEYSTONE_DBPASS | sed -i "s/KEYSTONE_DBPASS/${KEYSTONE_DBPASS}/g" /root/openstack/installkeystone.sls
 echo $GLANCE_DBPASS | sed -i "s/GLANCE_DBPASS/${GLANCE_DBPASS}/g" /root/openstack/installglance.sls
@@ -16,7 +16,7 @@ echo $NEUTRON_DBPASS | sed -i "s/NEUTRON_DBPASS/${NEUTRON_DBPASS}/g" /root/opens
 echo $DASH_DBPASS | sed -i "s/DASH_DBPASS/${DASH_DBPASS}/g" /root/openstack/installdashboard.sls
 echo $CINDER_DBPASS | sed -i "s/CINDER_DBPASS/${CINDER_DBPASS}/g" /root/openstack/installcinder_master.sls /root/openstack/installcinder_slave.sls
 
-
+#格式 sh dbpass.sh $1 $2 $3 $4 $5 $6 $7 $8
 
 
 
