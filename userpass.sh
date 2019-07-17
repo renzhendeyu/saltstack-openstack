41d3924fa9be10643088ba094958fdcf7b126efd
renzhendeyu <39947336+renzhendeyu@users.noreply.github.com>
2019-07-17 11:03:42 +0800

    修改所有用户密码

--- userpass.sh
+++ userpass.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-ADMIN_PASS=$1
-myuser_PASS=$2
-GLANCE_PASS=$3
-NOVA_PASS=$4
-PLACEMENT_PASS=$5
-NEUTRON_PASS=$6
-METADATA_SECRET=$7
-CINDER_PASS=$8
-RABBIT_PASS=$10
+ADMIN_PASS=$1             #admin用户密码
+myuser_PASS=$2            #myuser用户密码
+GLANCE_PASS=$3            #glance用户密码
+NOVA_PASS=$4              #nova用户密码
+PLACEMENT_PASS=$5         #placement用户密码
+NEUTRON_PASS=$6           #neutron用户密码
+METADATA_SECRET=$7        #metadata元数据密码
+CINDER_PASS=$8            #cinder用户密码
+RABBIT_PASS=$10           #rabbitmq用户openstack的密码
 echo $ADMIN_PASS | sed -i "s/ADMIN_PASS/${ADMIN_PASS}/g" /root/openstack/installkeystone.sls
 echo $myuser_PASS |  sed -i "s/myuser_PASS/${myuser_PASS}/g" /root/openstack/installkeystone.sls
 echo $GLANCE_PASS |  sed -i "s/GLANCE_PASS/${GLANCE_PASS}/g" /root/openstack/installglance.sls
@@ -18,3 +18,4 @@ echo $METADATA_SECRET | sed -i "s/METADATA_SECRET/${METADATA_SECRET}/g" /root/op
 echo $CINDER_PASS | sed -i "s/CINDER_PASS/${CINDER_PASS}/g" /root/openstack/installcinder_master.sls /root/openstack/installcinder_slave.sls
 echo $RABBIT_PASS | sed -i "s/RABBIT_PASS/${RABBIT_PASS}/g" /root/openstack/installcinder_slave.sls /root/openstack/installcinder_master.sls /root/openstack/installnova_master.sls /root/openstack/installneu_slave.sls /root/openstack/installneu_master.sls /root/openstack/installnova_slave.sls /root/openstack/rely_prepare.sls
 
+#sh userpass.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
