c853d08b1943c5ad9270704157d03740739bdcd8
renzhendeyu <39947336+renzhendeyu@users.noreply.github.com>
2019-07-17 11:00:22 +0800

    修改节点名称ip
    修改控制、计算、存储节点的名称，ip地址以及网卡接口名称

--- hosts.sh
+++ hosts.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-controller=$1
-compute=$2
-cinder=$3
-controller_ip=$4
-compute_ip=$5
-cinder_ip=$6
-controller_eno=$7
-compute_eno=$8
+controller=$1           #输入控制节点名称
+compute=$2              #输入计算节点名称
+cinder=$3               #输入存储节点名称
+controller_ip=$4        #输入控制节点ip
+compute_ip=$5           #输入计算节点ip
+cinder_ip=$6            #输入存储节点ip
+controller_eno=$7       #输入控制节点网卡接口名称
+compute_eno=$8          #输入计算节点网卡接口名称
 echo $controller | sed -i "s/slave1/${controller}/g" /root/openstack/* /root/envprepare/*
 echo $compute | sed -i "s/slave2/${compute}/g" /root/openstack/* /root/envprepare/*
 echo $cinder | sed -i "s/slave3/${cinder}/g" /root/envprepare/* /root/openstack/*
@@ -15,3 +15,5 @@ echo $compute_ip | sed -i "s/compute_ip/${compute_ip}/g" /root/openstack/install
 echo $controller_eno | sed -i "s/enomaster/${controller_eno}/g" /root/openstack/controller.sls 
 echo $compute_eno | sed -i "s/enocompute/${compute_eno}/g" /root/openstack/installneu_slave.sls
 echo $cinder_ip | sed -i "s/cinder_ip/${cinder_ip}/g" /root/openstack/installcinder_slave.sls
+
+#sh hosts.sh $1 $2 $3 $4 $5 $6 $7 $8
